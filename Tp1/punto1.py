e7a5d7715d511a8510107e6df04dbdd6e233a672
Gabriel Federico Jose Gimenez Miguel <gabylan02000@hotmail.com>
2024-11-05 07:24:53 -0300

    update

--- Tp1/punto1.py
+++ Tp1/punto1.py
@@ -1,7 +1,12 @@
 import re
 
-# Diccionario de reemplazos
-reemplazos = {
+"""Programe una herramienta que pueda cifrar un texto segÃºn ciertas reglas de reemplazo (por ejemplo, todas las "la" por "1", las "un" por "2", etc.) y luego descifrarlo de nuevo usando expresiones regulares.
+Ejemplo:
+Entrada: "Hola Mundo"
+Cifrado: "Ho1 M2do"
+Descifrado: "Hola Mundo"""
+
+reemplazos = {  # Diccionario de reemplazos
     'la': '1',
     'un': '2',
     'do': '3',
@@ -11,43 +16,41 @@ reemplazos = {
     'una': '7',
     'prueba': '8',
     'texto': '9',
-    'cifrar': '10',
-    'descifrar': '11',
-    'ejemplo': '12',
-    'mensaje': '13',
-    'clave': '14',
-    'valor': '15',
-    'cadena': '16',
-    'entrada': '17',
-    'salida': '18',
+    'fundamentos': 'a',
+    'teoricos': 'b',
+    'noseQueMasPonerAca': 'c',
     
 }
 
-def cifrar(texto, reglas):
-    claves = reglas.keys() 
+def cifrar(texto, diccionario): # Funcion de cifrado
+    
+    claves = diccionario.keys() 
     patron = '|'.join(claves) 
-    reemplazo = lambda match: reglas[match.group(0)] # Crear una func de reemplazo
+    reemplazo = lambda match: diccionario[match.group()] # Crear una func de reemplazo
     texto_cifrado = re.sub(patron, reemplazo, texto) # Aplicar el reemplazo
     return texto_cifrado 
 
-def descifrar(texto, reglas):
-    valores = reglas.values() 
+def descifrar(texto, diccionario): # Funcion de descifrado
+    valores = diccionario.values() 
     patron = '|'.join(valores) 
-    reglas_invertidas = {v: k for k, v in reglas.items()} # Invertir el diccionario
-    reemplazo = lambda match: reglas_invertidas[match.group(0)] # Crear el reemplazo
+    dict_invertido = {v: k for k, v in diccionario.items()} # Invertir el diccionario
+    reemplazo = lambda match: dict_invertido[match.group()] # Crear el reemplazo
     texto_descifrado = re.sub(patron, reemplazo, texto) # Aplicar el reemplazo
     return texto_descifrado 
 
 def main():
     while True:
-        cadena = input('Ingrese una cadena o 0 para salir: ')
-        if cadena == '0':
-            break    
-        texto_cifrado = cifrar(cadena, reemplazos)
-        print(f"Cifrado: {texto_cifrado}")
+        try:
+            cadena = input('Ingrese una cadena o 0 para salir: ')
+            if cadena == '0':
+                break    
+            texto_cifrado = cifrar(cadena, reemplazos)
+            print(f"Cifrado: {texto_cifrado}")
 
-        texto_descifrado = descifrar(texto_cifrado, reemplazos)
-        print(f"Descifrado: {texto_descifrado}")
+            texto_descifrado = descifrar(texto_cifrado, reemplazos)
+            print(f"Descifrado: {texto_descifrado}")
+        except KeyboardInterrupt:
+            print("Chau")
 
 if __name__ == "__main__":
     main()
