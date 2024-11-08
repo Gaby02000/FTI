a1c23ab87294ef6155772f816e0b0f3c9a6cbd08
Gabriel Federico Jose Gimenez Miguel <gabylan02000@hotmail.com>
2024-11-07 21:52:10 -0300

    update

--- Tp2/main.py
+++ Tp2/main.py
@@ -1,12 +1,24 @@
 import turtle
 import random
+import platform
+from os import system
 
 anguloRotacion = 36  
-longitudLinea = 20
-screen = turtle.Screen()
-screen.setup(width=800, height=600)
-screen.title("TP2 Grupo 6")
-screen.bgcolor("black")
+longitudLinea = 0
+
+tamanioLinea={
+    1: 70,
+    2: 60,  
+    3: 50,
+    4: 45,
+    5: 40,
+    6: 35,
+    7: 30,
+    8: 25,
+    9: 20,
+    10: 10,
+    11: 5
+}
 
 reglas={
     'A': 'CF++DF----BF[-CF----AF]++',
@@ -30,7 +42,7 @@ def aplicarReglas(cadena, iteraciones):
                 nuevaCadena += x
         cadena = nuevaCadena
         i += 1
-    print(cadena)
+    #print(cadena)
     return cadena
 
 def dibujar(cadena,colores):
@@ -58,39 +70,79 @@ def dibujar(cadena,colores):
             turtle.setposition(posicion)
             turtle.setheading(heading)
             turtle.pendown()
+    print('Dibujo terminado')
 
+def limpiarConsola():
+    if platform.system() == "Windows":
+        system("cls")
+    else:
+        system("clear")
+
+def configPantalla():
+    screen = turtle.Screen()
+    screen.setup(width=1.0, height=1.0)  # Pantalla completa
+    screen.title("TP2 Grupo 6")
+    screen.bgcolor("black")
+    screen.cv._rootwindow.attributes("-fullscreen", True)  # Modo pantalla completa
+    return screen
+
+def centroPantalla(screen):
+    ancho = screen.window_width()
+    alto = screen.window_height()
+    x = ancho / 2
+    y = alto / 2
+    return x, y 
+
+def centrarDibujo(screen):
+    x, y = centroPantalla(screen)
+    turtle.penup()
+    turtle.goto(0, 0)
+    turtle.pendown()
 def configTurtle(velocidad):
     turtle.speed(velocidad)  
     turtle.pencolor('white')
     turtle.pensize(1)
 
 def menu():
+    global longitudLinea
     colores = input('Elegi dos colores separados por comas: ')
+    limpiarConsola()
     while True:
         velocidad = int(input('Elegi la velocidad de dibujo: '))
         if velocidad >= 0 and velocidad <= 10:
             break
         else:
+            limpiarConsola()
             print('Valor invalido.')
+    limpiarConsola()
     while True:
         iteraciones = int(input('Elegi la cantidad de iteraciones: '))
-        if iteraciones > 0:
+        if iteraciones in tamanioLinea:
+            longitudLinea = tamanioLinea[iteraciones]
             break
         else:
+            limpiarConsola()
             print('Valor invalido.')
+    limpiarConsola()
     return colores, iteraciones, velocidad
 
 if __name__ == '__main__':
+    
     x = input('desea agregar otra cadena inicial? (s/n): ')
     if x == 's':
         cadenaInicial = input('ingrese cadena inicial: ')
     else:
         cadenaInicial = "[B]++[B]++[B]++[B]++[B]"
+    limpiarConsola()
     colores, iteraciones, velocidad = menu()
+    screen = configPantalla()
     configTurtle(velocidad)
+    centrarDibujo(screen) 
+    print('Dibujando...')
     try:
-        cadena=aplicarReglas(cadenaInicial, iteraciones)
+        cadena = aplicarReglas(cadenaInicial, iteraciones)
         dibujar(cadena,colores)
         turtle.done() 
+        
     except KeyboardInterrupt as e:
         print("Chau")
